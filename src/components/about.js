da1eaccae89f967c711f2ec88e43a359858319a9
lordvic4real <lordvic4real@gmail.com>
2021-07-25 18:22:20 +0100

    fix image alt

--- src/components/about.js
+++ src/components/about.js
@@ -30,7 +30,11 @@ export default function About() {
         <img src={VicBg} alt="abt bg" className="absolute w-full" />
         <div className="p-10 lg:48 container mx-auto relative">
           <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20 ">
-            <img src={urlFor(author.authorImage).url()} className="w-32 h-32" />
+            <img
+              src={urlFor(author.authorImage).url()}
+              alt="my-pix"
+              className="w-32 h-32"
+            />
             <div className="text-lg flex flex-col justify-center">
               <h1 className="cursive text-6xl text-green-600 mb-4">
                 hey there i am{" "}
