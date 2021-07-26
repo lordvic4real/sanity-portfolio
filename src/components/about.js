81007641f4206e5f751e5d30ef9785488c71b36c
lordvic4real <lordvic4real@gmail.com>
2021-07-26 08:51:32 +0100

    hello fix about page

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import sanityClient from "../client.js";
-import VicBg from "../image/me3.jpeg";
+import AboutBg from "../image/me7.jpeg";
 import imageUrlBuilder from "@sanity/image-url";
 import BlockContent from "@sanity/block-content-to-react";
 
@@ -20,14 +20,14 @@ export default function About() {
      "authorImage": image.asset->url
    }`
       )
-      .then((data) => setAthor(data))
+      .then((data) => setAthor(data[0]))
       .catch(console.error);
   }, []);
   if (!author) return <div>Loading....</div>;
   return (
     <>
       <main className="relative">
-        <img src={VicBg} alt="abt bg" className="absolute w-full" />
+        <img src={AboutBg} alt="abt bg" className="absolute w-full" />
         <div className="p-10 lg:48 container mx-auto relative">
           <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-20 ">
             <img
@@ -36,11 +36,11 @@ export default function About() {
               className="w-32 h-32"
             />
             <div className="text-lg flex flex-col justify-center">
-              <h1 className="cursive text-6xl text-green-600 mb-4">
+              <h1 className="cursive text-6xl text-green-600 mb-4 ml-5">
                 hey there i am{" "}
-                <span className="text-green-100">{author.name}</span>
+                <span className="text-green-100 ml-5">{author.name}</span>
               </h1>
-              <div className="prose lg:prose-xl text-white">
+              <div className="prose lg:prose-xl text-white ml-5">
                 <BlockContent
                   blocks={author.bio}
                   projectId="gco5q2a4"
